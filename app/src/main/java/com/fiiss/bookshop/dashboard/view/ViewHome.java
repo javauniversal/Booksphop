02aa797abc7a144cd6c2634a2d584b0b921c2b5f
German Garcia <ggarcia@intergrupo.com>
2020-06-10 21:06:18 -0500

    first commit

---
+++ app/src/main/java/com/fiiss/bookshop/dashboard/view/ViewHome.java
@@ -0,0 +1,30 @@
+package com.fiiss.bookshop.dashboard.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.fiiss.bookshop.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class ViewHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+}
